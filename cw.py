a8b0a93dabbe9ed6eb266dfec380e9bac015b812
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-08 12:12:05 +0100

    Update cw.py

--- cw.py
+++ cw.py
@@ -1,35 +1,40 @@
 # Zadanie 1
 def gmean(*args):
-    wynik = 1
-    dlugosc = 0
+    result = 1
+    dl = 0
     for arg in args:
-        wynik *= arg
-        dlugosc += 1
-    wynik = wynik ** (1 / dlugosc)
-    return wynik
+        result *= arg
+        dl += 1
+    result = result ** (1 / dl)
+    return result
 
 
 def func(*args):
+    for i in range(1, len(args) + 1):
+        print(" ".join([str(item) for item in args[:i]]))
+
+
+def func2(*args):
     for index, item in enumerate(args, start=1):
         print(" ".join([str(item)] * index))
 
 
-def func2(*args):
-    for i in range(1, len(args) + 1):
-        print(" ".join([str(item) for item in args[:i]]))
+
 
 
 # Zadanie 2
 
 
 def my_max(a, b):
-    return a if a > b else b
+    if (a>b):
+        return a
+    return b
 
 
 def my_max(a, b, c):
-    if a > b and a > c:
+    if (a >= b and a >= c):
         return a
-    elif b > a and b > c:
+    elif (b >= a and b >= c):
         return b
     else:
         return c
@@ -38,62 +43,61 @@ def my_max(a, b, c):
 
 def my_max(*args):
     najwieksza = args[0]
-    for arg in args:
-        if arg > najwieksza:
-            najwieksza = arg
+    for i in args:
+        if i > najwieksza:
+            najwieksza = i
     return najwieksza
 
 
 
 # Zadanie 3
 
-rekurencyjne = 0
+liczba = 0;
 
-
-def fibonaccirek(liczba):
-    global rekurencyjne
-    if liczba <= 1:
-        return liczba
+def rekurencyjna(n):
+    global liczba
+    if n <= 1:
+        return n
     else:
-        rekurencyjne += 1
-        return fibonaccirek(liczba-1) + fibonaccirek(liczba-2)
+        liczba += 1
+        return rekurencyjna(liczba-1) + rekurencyjna(liczba-2)
 
 
-nierekurencyjne = 0
-
-
-def fibonaccinrek(liczba):
-    global nierekurencyjne
-    if liczba <= 1:
-        return liczba
+def nierekurencyjna(n):
+    global liczbab
+    if n <= 1:
+        return n
     a = 0
     b = 1
-    for _ in range(2, liczba + 1):
-        nierekurencyjne += 1
+    for i in range(2, n + 1):
+        liczbab += 1
         a, b = b, a + b
     return b
-
-
-
-def algortytmeuklidesa(liczba1, liczba2):
-    if liczba2 == 0:
-        return liczba1
-    else:
-        return algortytmeuklidesa(liczba2, liczba1 % liczba2)
+ 
 
 
 
 
+# Zadanie 4
 
-def skroculamek(a, b):
-    dzielnik = algortytmeuklidesa(a, b)
-    p = a / dzielnik
+def nwd (a, b):
+    while b:
+        a, b = b, a%b
+        return abs(a)
+
+
+
+
+def ulamek(a, b):
+    dzielnik = nwd(a, b)
+    p = a // dzielnik
     q = b / dzielnik
-    return p.__int__(), q.__int__()
+    return p, int(q)
 
 
 
 
+# Zadanie 5
 
 def silnia(n):
     if n > 1:
@@ -102,28 +106,19 @@ def silnia(n):
 
 
 def main():
+    print('zadanie 1')
     print(gmean(2, 2, 5, 7))
-    print('-----------')
     func(3, 'Ala', 5)
-    print('-----------')
     func2(3, 'Ala', 5)
-    print('-----------')
+    print('zadanie 2')
     print(my_max(1, 2))
-    print('-----------')
     print(my_max(4, 5, 3))
-    print('-----------')
     print(my_max(1, 2, 3, 4, 5, 62, 7, 8, 9, 10))
-    print('-----------')
-    print(fibonaccirek(2))  # dla 10: 88; dla 29: 832039; dla 100 program za długo liczy
-    print(rekurencyjne)
-    print('-----------')
-    print(fibonaccinrek(12))  # dla 10: 9; dla 29: 28; dla 100: 99
-    print(nierekurencyjne)
-    print('-----------')
+    print('zadanie 3')
+    print(rekurencyjna(2))
+    print(nierekurencyjna(12))
+    print('zadanie 4')
     print(algortytmeuklidesa(1989, 867))
-    print(skroculamek(1989, 867))
-    print('-----------')
+    print(ulamek(1989, 867))
+    print('zadanie 5')
     print(silnia(5))
-
-
-main()
