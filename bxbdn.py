c242584e901b8622c6bfc106a3a8863240fadbef
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-15 12:15:44 +0100

    Update bxbdn.py

--- bxbdn.py
+++ bxbdn.py
@@ -15,3 +15,24 @@ def abs(x):
         return x
     else:
         return -x
+
+
+
+
+
+def abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+def square_root(a, epsilon=0.00001):
+    x = a
+    nastepny_x = 0.5 * (x + a / x)
+    while abs(nastepny_x - x) < epsilon:
+        nastepny_x = 0.5 * (x + a / x)
+        x = nastepny_x
+        return x
+
+liczba = 25
+print(f"Pierwiastek kwadratowy z", liczba, "wynosi:", square_root(liczba))
