539356b3d963eb807e6fd4ffdafc2291e23369e3
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-27 19:56:31 +0100

    Add files via upload

---
+++ zadania/03/zadanie_02/zad_02_c.py
@@ -0,0 +1,11 @@
+liczba1 = -1
+liczba2 = -1
+liczba3 = -1
+while (liczba1<0 or liczba2<0 or liczba3<0 ):
+    liczba1 = int(input("Podaj liczbe calkowita: "))
+    liczba2 = int(input("Podaj liczbe calkowita: "))
+    liczba3 = int(input("Podaj liczbe calkowita: "))
+
+
+if(liczba1 == 0 or liczba2 == 0 or liczba3 == 0):
+    print("odpowiedni komunikat")
